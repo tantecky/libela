90d2b7c3b36b178fd7693928a65166e9006761b7
tantecky <tomas@antecky.cz>
2023-12-21 13:31:59 +0100

    Add SSL support

--- src/components/Orientation.tsx
+++ src/components/Orientation.tsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from 'react'
+
+interface OrientationData {
+  alpha: number | null
+  beta: number | null
+  gamma: number | null
+}
+
 export default function Orientation() {
-  return <p>aa</p>
+  const [orientationData, setOrientationData] = useState<OrientationData>()
+
+  useEffect(() => {
+    window.addEventListener(
+      'deviceorientation',
+      (event: DeviceOrientationEvent) => {
+        setOrientationData({
+          alpha: event.alpha,
+          beta: event.beta,
+          gamma: event.gamma,
+        })
+      },
+      true,
+    )
+  })
+
+  return (
+    <>
+      <p>alpha:{orientationData?.alpha}</p>
+      <p>beta:{orientationData?.beta}</p>
+      <p>gamma:{orientationData?.gamma}</p>
+    </>
+  )
 }
