9b3d1ca36579c7c0709e7f835fa9c17bb8df3c34
tantecky <tomas@antecky.cz>
2023-12-23 06:25:12 +0100

    Add PWA

---
+++ run_http_server.bat
@@ -0,0 +1,3 @@
+@echo off
+cd dist
+npx http-server
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,50 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        // enabled: true,
+      },
+      manifest: {
+        name: 'Libela',
+        short_name: 'Libela',
+        description: 'Simple bubble level for your phone',
+        theme_color: '#000000',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
